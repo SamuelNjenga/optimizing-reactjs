05ba3ad1af0b3b62e87faeea8573b976cd6e4b35
Samuel Njenga <snjenga704@gmail.com>
2020-06-16 15:10:02 +0300

    Updated ErrorBoundary.js by including componentDidCatch lifecycle method

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -16,6 +16,11 @@ class ErrorBoundary extends Component {
             hasError: true
         }
     }
+
+    componentDidCatch(error,info){
+        console.log(error)
+        console.log(info)
+    }
     render() {
         if(this.state.hasError){
             return <h1>Something went wrong</h1>
