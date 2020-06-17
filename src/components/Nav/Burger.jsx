a99125991a823e682d70af2abea88f72064218aa
Samuel Njenga <snjenga704@gmail.com>
2020-06-17 15:58:23 +0300

    Included Animation  style to the Burger.jsx component

--- src/components/Nav/Burger.jsx
+++ src/components/Nav/Burger.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components';
 
 const StyledBurger = styled.div`
@@ -13,13 +13,29 @@ flex-flow:column nowrap;
 div{
     width:2rem;
     height:0.25rem;
-    background-color:#333;
+    background-color: ${({open}) => open ? '#ccc':'#333'};
     border-radius:10px;
+    transform-origin:1px;
+    transition: all 0.3s linear;
+
+    &:nth-child(1) {
+        transform: ${({open}) => open ? 'rotate(45deg)' : 'rotate(0)'}
+    }
+
+    &:nth-child(2) {
+        transform: ${({open}) => open ? 'translateX(100%)' : 'translateX(0)'};
+        opacity: ${({open}) => open ? 0 : 1};
+    }
+
+    &:nth-child(3) {
+        transform: ${({open}) => open ? 'rotate(-45deg)' : 'rotate(0)'}
+    }
 }
 `
 const Burger = () => {
+    const [open,setOpen] = useState(false);
   return (
-    <StyledBurger>
+    <StyledBurger open={open} onClick = {() => setOpen(!open)}>
       <div />
       <div />
       <div />
