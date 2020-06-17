b858ecbe952b717afd038cfc8307a27bf4dc8153
Samuel Njenga <snjenga704@gmail.com>
2020-06-17 14:48:09 +0300

    Initial styling of Navbar.jsx

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import styled from 'styled-components';
 const Nav = styled.nav`
-  width:100%;
-  height:65px;
+  width: 100%;
+  height: 65px;
   border-bottom: 2px solid #f1f1f1;
-  padding:0 20px;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+
+  .logo{
+      padding: 15px 0;
+  }
+  ul{
+      list-style: none;
+      display:flex;
+      flex-flow:row no-wrap
+  }
+  li{
+      padding:18px 10px;
+  }
 `
 const Navbar = () => {
   return (
