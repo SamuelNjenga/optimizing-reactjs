d321a6899374e4272f5d4eecebd9c8ada1666d79
Samuel Njenga <snjenga704@gmail.com>
2020-06-17 13:55:51 +0300

    Initial set up  for the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <h1>Exploring more on React js Optimizations via separate github branches</h1>
+      <h2>Jumbo</h2>
     </div>
   );
 }
